2c9f7463fcfcfa49e2c293f4d0777ef7e6015528
Nandhika Prayoga <nann011530@gmail.com>
2020-09-21 20:15:55 +0700

    Complete first week task

---
+++ app/src/main/java/id/ui/ac/cs/mobileprogramming/nandhikaprayoga/helloworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ui.ac.cs.mobileprogramming.nandhikaprayoga.helloworld
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
