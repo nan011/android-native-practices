148e588e42f39dc39d331042ef100e6b8c26cc9e
Nandhika Prayoga <nann011530@gmail.com>
2020-10-05 05:38:32 +0700

    Override back button, add custom back button on UI, and create counter view

--- app/src/main/java/id/ui/ac/cs/mobileprogramming/nandhikaprayoga/MainActivity.kt
+++ app/src/main/java/id/ui/ac/cs/mobileprogramming/nandhikaprayoga/MainActivity.kt
@@ -1,12 +1,23 @@
 package id.ui.ac.cs.mobileprogramming.nandhikaprayoga
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import id.ui.ac.cs.mobileprogramming.nandhikaprayoga.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        exitButton.setOnClickListener {
+            super.onBackPressed()
+        }
+
+    }
+
+    override fun onBackPressed() {
+        Toast.makeText(this@MainActivity, "Yoo I override this back pressed haha", Toast.LENGTH_LONG).show()
     }
 }
